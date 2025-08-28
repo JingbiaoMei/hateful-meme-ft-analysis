8578fa296a37ddca621d7749ba96b8fe8b065ba4
jingbiaomei <jingbiaomei@gmail.com>
2025-08-28 16:44:28 +0100

    add

--- scripts/dpo/hm/qwen25vl_3b_dpo.sh
+++ scripts/dpo/hm/qwen25vl_3b_dpo.sh
@@ -1,1 +1,9 @@
-llamafactory-cli train scripts/dpo/hm/qwen2_5vl_3b_lora_dpo.yaml
+model_name=qwen2_5vl_3b
+dataset_name=hm
+current_date=$(date +'%Y-%m-%d')
+export WANDB_PROJECT="LLAMAFACTORY_hateful_DPO"
+export WANDB_NAME=$Name
+export WANDB_RUN_GROUP="Finetuning_${model_name}_${dataset_name}_${current_date}"
+
+
+llamafactory-cli train scripts/dpo/${dataset_name}/${model_name}_lora_dpo.yaml
